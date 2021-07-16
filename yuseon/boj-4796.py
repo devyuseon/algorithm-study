2e9730de332feb39384005c4902059c508a20400
임유선 <1991167@hansung.ac.kr>
2021-07-16 23:07:44 +0900

    Add: 백준 4796 다른 풀이 추가

--- yuseon/boj-4796.py
+++ yuseon/boj-4796.py
@@ -14,9 +14,26 @@ def solution(L: int, P: int, V: int):
                 result += V
             return result
 
+# 두번째 풀이 (나눗셈)
+
+def solution2(L: int, P: int, V: int):
+    result = 0
+    result += (V // P) * L
+
+    carry = V % P
+    V -= (V // P) * P
+    
+    if carry > L:
+        return result + L
+    else:
+        return result + V
+    
+
+
 i = 1
 while True:
     L, P, V = map(int, sys.stdin.readline().split())
     if V == 0: break
-    print(f"Case {i}: {solution(L, P, V)}")
+    # print(f"Case {i}: {solution(L, P, V)}")
+    print(f"Case {i}: {solution2(L, P, V)}")
     i += 1
